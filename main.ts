2bcca3a4e51478070469fb841abf3ddb2fe838ab
1711095 <104539662+1711095@users.noreply.github.com>
2022-05-04 12:28:56 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(6)
 })
